59ae75295abbfd1d3af26db0423fec1efd4a03b1
bsdaoquang <bsdaoquang@gmail.com>
2024-06-13 11:31:18 +0700

    add login screen

--- src/screens/auth/Login.tsx
+++ src/screens/auth/Login.tsx
@@ -1,14 +1,67 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Container from '../../components/Container';
 import {Text} from '../../components';
-import {Section} from '@bsdaoquang/rncomponent';
+import {Button, Input, Loading, Section} from '@bsdaoquang/rncomponent';
+import {fontFamilies} from '../../constants/fontFamilies';
+import auth from '@react-native-firebase/auth';
 
-const Login = () => {
+const Login = ({navigation}: any) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    if (email && password) {
+      setIsLoading(true);
+      try {
+        const user = await auth().signInWithEmailAndPassword(email, password);
+
+        if (user) {
+          console.log(user);
+        }
+
+        setIsLoading(false);
+      } catch (error) {
+        setIsLoading(false);
+        console.log(error);
+      }
+    } else {
+      console.log('Error');
+    }
+  };
   return (
     <Container>
       <Section>
-        <Text text="fafa" />
+        <Text text="Login" size={28} font={fontFamilies.RobotoBold} />
       </Section>
+      <Section>
+        <Input
+          value={email}
+          onChange={val => setEmail(val)}
+          placeholder="Email"
+          keyboardType="email-address"
+          required
+          helpText="Nhập email"
+        />
+        <Input
+          value={password}
+          password
+          onChange={val => setPassword(val)}
+          placeholder="Mật khẩu"
+          required
+          helpText="Nhập mật khẩu"
+        />
+      </Section>
+      <Section>
+        <Button
+          type="primary"
+          onPress={handleLogin}
+          title="Đăng nhập"
+          textStyleProps={{fontSize: 16}}
+          styles={{paddingVertical: 8}}
+        />
+      </Section>
+      <Loading loading={isLoading} />
     </Container>
   );
 };
--- src/screens/home/HomeScreen.tsx
+++ src/screens/home/HomeScreen.tsx
@@ -1,10 +1,11 @@
-import {View, Text} from 'react-native';
+import {View} from 'react-native';
 import React from 'react';
+import TextComponent from '../../components/TextComponent';
 
 const HomeScreen = () => {
   return (
-    <View>
-      <Text>HomeScreen</Text>
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <TextComponent text="Map view" />
     </View>
   );
 };
